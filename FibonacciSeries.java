bba4d12b45fd2531642fc20eddcb4fdbe25ceaae
SubburajArumugaperumal <subburajsanju@gmail.com>
2024-07-26 23:26:13 +0530

    Add files via upload
    Week one assignments

---
+++ FibonacciSeries.java
@@ -0,0 +1,15 @@
+package assignment.jul20;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 1;
+		System.out.println(a + "\n" + b);
+		for (int i = 3; i <= 8; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+			System.out.println(c);
+		}
+	}
+}
