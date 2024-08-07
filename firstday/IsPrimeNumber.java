59a3759de15a4f3bd6ddf914192b0c6ac9c65f5a
SubburajArumugaperumal <subburajsanju@gmail.com>
2024-08-07 22:21:04 +0530

    Add files via upload

---
+++ firstday/IsPrimeNumber.java
@@ -0,0 +1,22 @@
+package assignment.firstday;
+
+public class IsPrimeNumber {
+public static void main(String[] args) {
+	int n = 4;
+	boolean isPrime = true;
+	
+	for (int i = 2; i < n; i++) {
+		if (n % i == 0) {
+			isPrime = false;
+			break;
+		}
+	}
+	
+	if(isPrime) {
+		System.out.println(n + " is Prime");
+	} else {
+		System.out.println(n + " is not Prime");
+	}
+}
+}
+
---
+++ secondday/LibraryManagement.java
@@ -0,0 +1,12 @@
+package assignment.secondday;
+
+public class LibraryManagement {
+	public static void main(String[] args) {
+		Library a = new Library();
+		String booktitle = ("ramayanam");
+		a.addBook(booktitle);
+		a.issueBook();
+
+	}
+
+}
